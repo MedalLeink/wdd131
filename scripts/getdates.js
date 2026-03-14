7e733fb65885a469cf141e169d524bcf8acdab6d
Madeleine Nkiru <anaghamadeleine@gmail.com>
2026-03-14 18:25:02 -0400

    css and javascript added

---
+++ scripts/getdates.js
@@ -0,0 +1,11 @@
+// getdates.js
+// WDD 131 – Dynamic Web Fundamentals
+// Author: Your Name
+// Purpose: Dynamically populate the copyright year and last-modified date in the footer.
+
+// 1. Current year for the copyright notice
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").innerHTML = currentYear;
+
+// 2. Date the document was last modified
+document.getElementById("lastModified").innerHTML = "Last Modification: " + document.lastModified;
